9ced340841eec52c4c5f8e3baec322dd7c3a5143
HakTak <katek.m536@gmail.com>
2025-10-08 17:21:24 +0200

    comment limit set

--- isa-backend/src/main/java/rs/ac/uns/ftn/informatika/jpa/repository/CommentRepository.java
+++ isa-backend/src/main/java/rs/ac/uns/ftn/informatika/jpa/repository/CommentRepository.java
@@ -24,4 +24,8 @@ public interface CommentRepository extends JpaRepository<Comment,Integer> {
     @Query("SELECT EXTRACT(YEAR FROM c.createdAt) AS year, COUNT(c) AS count " +
             "FROM Comment c GROUP BY EXTRACT(YEAR FROM c.createdAt) ORDER BY year")
     List<Map<String, Object>> countCommentsByYear();
+
+    @Query(value = "SELECT COUNT(*) FROM public.comments c WHERE c.creation_time >= NOW() - INTERVAL '2 minutes'" + //3.10 60 minuta
+            "AND c.user_id = :authorId", nativeQuery = true)
+    Long countCommentFromOneHourAgo(Integer authorId);
 }
